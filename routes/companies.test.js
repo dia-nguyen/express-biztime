669a542e01a01e8b25e353cb752a0d9ed77b1690
Diane <hello@dianenguyen.ca>
2023-01-06 09:20:18 -0800

    Added test for creating a new company

--- routes/companies.test.js
+++ routes/companies.test.js
@@ -1,4 +1,4 @@
-'use strict'
+"use strict";
 
 const db = require("../db");
 const request = require("supertest");
@@ -6,27 +6,45 @@ const app = require("../app");
 
 let amazonTestCompany;
 
-beforeEach( async function() {
+beforeEach(async function () {
   await db.query("DELETE FROM companies");
   let result = await db.query(`
   INSERT INTO companies (code,name,description)
   VALUES ('amazon', 'Amazon', 'Shipper of things')
-  RETURNING code, name, description`)
+  RETURNING code, name, description`);
   amazonTestCompany = result.rows[0];
 });
 
-
 /* GET /companies - returns `{companies: [amazon, ...]}` */
-describe("GET /companies", function() {
+describe("GET /companies", function () {
   test("Gets a list of 2 companies", async () => {
     const resp = await request(app).get("/companies");
 
     delete amazonTestCompany.description;
 
     expect(resp.body).toEqual({
-      "companies": [
-          amazonTestCompany
-      ]
-    })
-  })
-})
+      companies: [amazonTestCompany],
+    });
+  });
+});
+
+/**POST /companies - returns  {company: {code, name, description}}*/
+describe("POST /companies", function () {
+  test("Adds a company", async () => {
+    let newCompany = {
+      name: "microsoft",
+      code: "microsoft",
+      description: "it's alright",
+    };
+
+    const resp = await request(app)
+      .post("/companies")
+      .send(newCompany);
+
+    expect(resp.statusCode).toEqual(201);
+
+    expect(resp.body).toEqual({
+      company:  newCompany,
+    });
+  });
+});
